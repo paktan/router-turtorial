fa893fa860fced3e40e1db0e71f3ed60b3dc20dd
paktan <pakan@gmail.com>
2019-10-07 15:52:25 +0900

    react history

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import Home from './Home'
 import About from './About'
 import Profiles from './Profiles'
+import HistorySample from './HistorySample'
 
 function App() {
   return (
@@ -18,10 +19,14 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true}></Route>
       <Route path={['/about', '/info']} component={About}></Route>
       <Route path="/profiles" component={Profiles}></Route>
+      <Route path="/history" component={HistorySample}></Route>
     </div>
   )
 }
