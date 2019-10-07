80d305e3e0cf64fd7ae68717d2f67824d609efcb
paktan <pakan@gmail.com>
2019-10-07 15:14:30 +0900

    router start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+import Home from './Home'
+import About from './About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} exact={true}></Route>
+      <Route path={["/about", "/info"]}component={About}></Route>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
