8beddbb7cee96006b2728ec71ee72c43b6c0a86a
paktan <pakan@gmail.com>
2019-10-07 16:29:26 +0900

    navlink add

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import {Link, Route} from "react-router-dom"
-import Profile from "./Profile"
-import WithRouterSample from './WithRouterSample';
+import React from 'react'
+import { NavLink, Route } from 'react-router-dom'
+import Profile from './Profile'
+import WithRouterSample from './WithRouterSample'
 
 const Profiles = () => {
-    return (
-        <div>
-            <h3>사용자 목록:</h3>
-            <ul>
-                <li>
-                    <Link to="/profiles/velopert">velopert</Link>
-                </li>
-                <li>
-                    <Link to="/profiles/gildong">gildong</Link>
-                </li>
-            </ul>
-            <Route path="/profiles" exact render={()=><div>사용자를 선택해 주세요. </div>}></Route>
-            <Route path="/profiles/:username" component={Profile}></Route>
-        </div>
-    );
-};
+  const activeStyle = {
+    background: 'black',
+    color: 'white',
+  }
+  return (
+    <div>
+      <h3>사용자 목록:</h3>
+      <ul>
+        <li>
+          <NavLink activeStyle={activeStyle} to="/profiles/velopert">
+            velopert
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={activeStyle} to="/profiles/gildong">
+            gildong
+          </NavLink>
+        </li>
+      </ul>
+      <Route
+        path="/profiles"
+        exact
+        render={() => <div>사용자를 선택해 주세요. </div>}
+      ></Route>
+      <Route path="/profiles/:username" component={Profile}></Route>
+    </div>
+  )
+}
 
-export default Profiles;
+export default Profiles
